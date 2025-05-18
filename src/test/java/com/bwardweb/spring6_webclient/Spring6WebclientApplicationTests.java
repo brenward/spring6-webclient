ca8cc640619a4a07e9d8a44a063d7017c32a7bf2
brenward <bren.ward@gmail.com>
2025-05-18 13:58:43 +0100

    first commit

---
+++ src/test/java/com/bwardweb/spring6_webclient/Spring6WebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwardweb.spring6_webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
