47993ae34cbd14cb7384d1b8131ba741b49a7a31
brenward <bren.ward@gmail.com>
2025-05-18 14:18:00 +0100

    #Get using JsonNode

--- src/test/java/com/bwardweb/spring6_webclient/client/BeerClientImplTest.java
+++ src/test/java/com/bwardweb/spring6_webclient/client/BeerClientImplTest.java
@@ -28,11 +28,23 @@ class BeerClientImplTest {
     }
 
     @Test
-    void listBeerMap(){
+    void testGetMap(){
         AtomicBoolean atomicBoolean = new AtomicBoolean(false);
 
         client.listBeersMap().subscribe(response -> {
             System.out.println(response);
+            atomicBoolean.set(true);
+        });
+
+        await().untilTrue(atomicBoolean);
+    }
+
+    @Test
+    void testGetBeerJson(){
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+
+        client.listBeersJsonNode().subscribe(jsonNode -> {
+            System.out.println(jsonNode.toPrettyString());
             atomicBoolean.set(true);
         });
 
